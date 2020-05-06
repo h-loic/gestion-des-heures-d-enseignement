3adff65ec7a5d179630efa63aaf0414686131d7b
h-loic <h-loic@orange.fr>
2020-05-06 12:18:31 +0200

    FRONTEND creation component accueil et ajout des boutons

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
