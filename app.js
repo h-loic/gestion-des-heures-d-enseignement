1db3f9b794675e2cc537e972d61aeaa682a278e0
h-loic <h-loic@orange.fr>
2020-05-08 09:16:57 +0200

    modification ordre use pour le fonctionnement des images

--- app.js
+++ app.js
@@ -24,11 +24,10 @@ app.use(bodyParser.urlencoded());
 app.use(upload.single('file'));
 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/images',express.static(__dirname + '/images'));
 app.use('/', require('./routes/statuts'));
 app.use('/', require('./routes/enseignants'));
 app.use('/', require('./routes/accueil'));
-
-app.use('/images',express.static(__dirname + '/images'));
 
 nunjucks.configure('views',{
     autoescape: true,
