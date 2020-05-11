cc996bddcb42e67827625af79ea876740f898215
h-loic <h-loic@orange.fr>
2020-05-11 14:03:46 +0200

    CRUD periode (subdocument de formation)

--- models/Formation.js
+++ models/Formation.js
@@ -1,20 +1,17 @@
 var mongoose = require('mongoose');
 
+var periode = new mongoose.Schema({
+    nom_periode : String,
+    duree : Number,
+    nombre_groupe_CM : Number,
+    nombre_groupe_TD : Number,
+    nombre_groupe_TP : Number,
+    nombre_groupe_Partiel : Number,
+})
 var formationSchema = new mongoose.Schema({
     nom : String,
     surnom : String,
-    periode : [{
-        nom_periode : String,
-        duree : Number,
-        nombre_groupe_CM : Number,
-        nombre_groupe_TD : Number,
-        nombre_groupe_TP : Number,
-        nombre_groupe_Partiel : Number,
-        decomposition : [{
-            nom_decomposition : String,
-            niveau : Number
-        }]
-    }]
+    periode : [periode]
 });
 
 var Formation = mongoose.model('Formation',formationSchema);
--- routes/formations.js
+++ routes/formations.js
@@ -1,6 +1,49 @@
 var router = require('express').Router();
 var Formation = require('./../models/Formation');
 
+router.get('/formation/delete-periode/:id/:idPeriode', (req, res) => {
+    Formation.findById(req.params.id).then(formation => {
+        formation.periode.id(req.params.idPeriode).remove();
+        formation.save();
+        res.redirect('/formation');
+    });
+});
+
+router.get('/formation/add-periode/:id', (req,res) => {
+    Formation.findById(req.params.id).then(formation => {
+        res.render('formations/edit-periode.html', { formation : formation, endpoint: '/formation/add-periode/' + formation._id.toString()});
+    });
+});
+
+router.get('/formation/edit-periode/:id/:idPeriode', (req,res) => {
+    Formation.findById(req.params.id).then(formation => {
+        let periode = formation.periode.id(req.params.idPeriode);
+        res.render('formations/edit-periode.html', { periode :periode , endpoint: '/formation/edit-periode/' + formation._id.toString() + '/' + periode._id.toString()});
+    });
+});
+
+router.post('/formation/edit-periode/:id/:idPeriode', (req, res) => {
+    Formation.findById(req.params.id).then(formation => {
+        let periode = formation.periode.id(req.params.idPeriode);
+        periode.nom_periode = req.body.nom ;
+        return formation.save();
+    }).then(() => {
+        res.redirect('/formation');
+    }, err => console.log(err));
+});
+
+router.post('/formation/add-periode/:id?', (req, res) => {
+    Formation.findById(req.params.id).then(formation => {
+        formation.periode.push({
+            nom_periode : req.body.nom
+        });
+        return formation.save();
+    }).then(() => {
+        res.redirect('/formation');
+    }, err => console.log(err));
+});
+
+
 router.get('/formation/new', (req,res) => {
     var formation = new Formation();
     res.render('formations/edit.html', { formation : formation, endpoint: '/formation'});
