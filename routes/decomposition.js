9373e43ac4e3e08f0423f612fdc90a01ecdd1d5a
h-loic <h-loic@orange.fr>
2020-05-25 15:50:15 +0200

    bilan ajout heure supp

--- routes/decomposition.js
+++ routes/decomposition.js
@@ -41,91 +41,98 @@ router.post('/projet/decomposition/record-global/:idProjet/:idFormation/:idDecom
 
 router.post('/projet/decomposition/intervenant/record/:idProjet/:idFormation/:duree/:idDecomposition/:idIntervenant', (req, res) => {
     Projet.findById(req.params.idProjet).populate('intervenants').then(projet => {
-        let decomposition = projet.decomposition.id(req.params.idDecomposition);
-        let intervenant_decomposition  = decomposition.intervenants.id(req.params.idIntervenant);
-        let duree = req.params.duree;
-        let total = 0;
-        duree -= 1;
+        projet.populate('intervenants.enseignant').execPopulate().then(projet => {
+            projet.populate('intervenants.enseignant.statut').execPopulate().then(projet => {
+                let decomposition = projet.decomposition.id(req.params.idDecomposition);
+                let intervenant_decomposition = decomposition.intervenants.id(req.params.idIntervenant);
+                let duree = req.params.duree;
+                let total = 0;
+                duree -= 1;
 
-        let index_intervenant;
-        for (let i = 0; i < projet.intervenants.length ; i++){
-            if (projet.intervenants[i]._id.toString() === intervenant_decomposition.intervenant.toString()){
-                index_intervenant = i;
-            }
-        }
-        if (intervenant_decomposition.nombre_heure_CM[duree] !== 'undefined'){
-            projet.intervenants[index_intervenant].nombre_heure_CM -= intervenant_decomposition.nombre_heure_CM[duree];
-            total += intervenant_decomposition.nombre_heure_CM[duree];
-        }
-        if (intervenant_decomposition.nombre_heure_TD[duree] !== 'undefined'){
-            projet.intervenants[index_intervenant].nombre_heure_TD -= intervenant_decomposition.nombre_heure_TD[duree];
-            total += intervenant_decomposition.nombre_heure_TD[duree];
-        }
-        if (intervenant_decomposition.nombre_heure_TP[duree] !== 'undefined'){
-            projet.intervenants[index_intervenant].nombre_heure_TP -= intervenant_decomposition.nombre_heure_TP[duree];
-            total += intervenant_decomposition.nombre_heure_TP[duree];
-        }
-        if (intervenant_decomposition.nombre_heure_Partiel[duree] !== 'undefined'){
-            projet.intervenants[index_intervenant].nombre_heure_Partiel -= intervenant_decomposition.nombre_heure_Partiel[duree];
-            total += intervenant_decomposition.nombre_heure_Partiel[duree];
-        }
+                let index_intervenant;
+                for (let i = 0; i < projet.intervenants.length; i++) {
+                    if (projet.intervenants[i]._id.toString() === intervenant_decomposition.intervenant.toString()) {
+                        index_intervenant = i;
+                    }
+                }
+                if (intervenant_decomposition.nombre_heure_CM[duree] !== 'undefined') {
+                    projet.intervenants[index_intervenant].nombre_heure_CM -= intervenant_decomposition.nombre_heure_CM[duree];
+                    total += intervenant_decomposition.nombre_heure_CM[duree];
+                }
+                if (intervenant_decomposition.nombre_heure_TD[duree] !== 'undefined') {
+                    projet.intervenants[index_intervenant].nombre_heure_TD -= intervenant_decomposition.nombre_heure_TD[duree];
+                    total += intervenant_decomposition.nombre_heure_TD[duree];
+                }
+                if (intervenant_decomposition.nombre_heure_TP[duree] !== 'undefined') {
+                    projet.intervenants[index_intervenant].nombre_heure_TP -= intervenant_decomposition.nombre_heure_TP[duree];
+                    total += intervenant_decomposition.nombre_heure_TP[duree];
+                }
+                if (intervenant_decomposition.nombre_heure_Partiel[duree] !== 'undefined') {
+                    projet.intervenants[index_intervenant].nombre_heure_Partiel -= intervenant_decomposition.nombre_heure_Partiel[duree];
+                    total += intervenant_decomposition.nombre_heure_Partiel[duree];
+                }
 
-        projet.intervenants[index_intervenant].nombre_heure_total -= total;
+                projet.intervenants[index_intervenant].nombre_heure_total -= total;
 
 
-        let heure_CM = [];
-        let heure_TD = [];
-        let heure_TP = [];
-        let heure_Partiel = [];
-        let somme_heure_CM = 0;
-        let somme_heure_TD = 0;
-        let somme_heure_TP = 0;
-        let somme_heure_Partiel = 0;
-        total = 0;
+                let heure_CM = [];
+                let heure_TD = [];
+                let heure_TP = [];
+                let heure_Partiel = [];
+                let somme_heure_CM = 0;
+                let somme_heure_TD = 0;
+                let somme_heure_TP = 0;
+                let somme_heure_Partiel = 0;
+                total = 0;
 
-        for (let i = 0 ; i < duree; i++){
-            if (isNaN(req.body.nombre_heure_CM[i]) || req.body.nombre_heure_CM[i] === '0' || req.body.nombre_heure_CM[i] === ''){
-                heure_CM.push(0);
-            }else{
-                heure_CM.push(req.body.nombre_heure_CM[i]);
-                somme_heure_CM += parseInt(req.body.nombre_heure_CM[i]);
-            }
-            if (isNaN(req.body.nombre_heure_TD[i]) || req.body.nombre_heure_TD[i] === '0' || req.body.nombre_heure_TD[i] === ''){
-                heure_TD.push(0);
-            }else{
-                heure_TD.push(req.body.nombre_heure_TD[i]);
-                somme_heure_TD += parseInt(req.body.nombre_heure_TD[i]);
-            }
-            if (isNaN(req.body.nombre_heure_TP[i]) || req.body.nombre_heure_TP[i] === '0' || req.body.nombre_heure_TP[i] === ''){
-                heure_TP.push(0);
-            }else{
-                heure_TP.push(req.body.nombre_heure_TP[i]);
-                somme_heure_TP += parseInt(req.body.nombre_heure_TP[i]);
-            }
-            if (isNaN(req.body.nombre_heure_Partiel[i]) || req.body.nombre_heure_Partiel[i] === '0' || req.body.nombre_heure_Partiel[i] === ''){
-                heure_Partiel.push(0);
-            }else{
-                heure_Partiel.push(req.body.nombre_heure_Partiel[i]);
-                somme_heure_Partiel += parseInt(req.body.nombre_heure_Partiel[i]);
-            }
-        }
-        total = somme_heure_Partiel + somme_heure_CM +somme_heure_TP +somme_heure_TD;
-        projet.intervenants[index_intervenant].nombre_heure_CM += somme_heure_CM;
-        projet.intervenants[index_intervenant].nombre_heure_TD += somme_heure_TD;
-        projet.intervenants[index_intervenant].nombre_heure_TP += somme_heure_TP;
-        projet.intervenants[index_intervenant].nombre_heure_Partiel += somme_heure_Partiel;
-        projet.intervenants[index_intervenant].nombre_heure_total += total;
-        projet.intervenants[index_intervenant].save();
-        heure_CM.push(somme_heure_CM);
-        heure_TD.push(somme_heure_TD);
-        heure_TP.push(somme_heure_TP);
-        heure_Partiel.push(somme_heure_Partiel);
+                for (let i = 0; i < duree; i++) {
+                    if (isNaN(req.body.nombre_heure_CM[i]) || req.body.nombre_heure_CM[i] === '0' || req.body.nombre_heure_CM[i] === '') {
+                        heure_CM.push(0);
+                    } else {
+                        heure_CM.push(req.body.nombre_heure_CM[i]);
+                        somme_heure_CM += parseInt(req.body.nombre_heure_CM[i]);
+                    }
+                    if (isNaN(req.body.nombre_heure_TD[i]) || req.body.nombre_heure_TD[i] === '0' || req.body.nombre_heure_TD[i] === '') {
+                        heure_TD.push(0);
+                    } else {
+                        heure_TD.push(req.body.nombre_heure_TD[i]);
+                        somme_heure_TD += parseInt(req.body.nombre_heure_TD[i]);
+                    }
+                    if (isNaN(req.body.nombre_heure_TP[i]) || req.body.nombre_heure_TP[i] === '0' || req.body.nombre_heure_TP[i] === '') {
+                        heure_TP.push(0);
+                    } else {
+                        heure_TP.push(req.body.nombre_heure_TP[i]);
+                        somme_heure_TP += parseInt(req.body.nombre_heure_TP[i]);
+                    }
+                    if (isNaN(req.body.nombre_heure_Partiel[i]) || req.body.nombre_heure_Partiel[i] === '0' || req.body.nombre_heure_Partiel[i] === '') {
+                        heure_Partiel.push(0);
+                    } else {
+                        heure_Partiel.push(req.body.nombre_heure_Partiel[i]);
+                        somme_heure_Partiel += parseInt(req.body.nombre_heure_Partiel[i]);
+                    }
+                }
+                total = somme_heure_Partiel + somme_heure_CM + somme_heure_TP + somme_heure_TD;
+                projet.intervenants[index_intervenant].nombre_heure_CM += somme_heure_CM;
+                projet.intervenants[index_intervenant].nombre_heure_TD += somme_heure_TD;
+                projet.intervenants[index_intervenant].nombre_heure_TP += somme_heure_TP;
+                projet.intervenants[index_intervenant].nombre_heure_Partiel += somme_heure_Partiel;
+                projet.intervenants[index_intervenant].nombre_heure_total += total;
+                if (projet.intervenants[index_intervenant].nombre_heure_total > projet.intervenants[index_intervenant].enseignant.statut.heure_normal_max) {
+                    projet.intervenants[index_intervenant].nombre_heure_HeSup = projet.intervenants[index_intervenant].nombre_heure_total - projet.intervenants[index_intervenant].enseignant.statut.heure_normal_max;
+                }
+                projet.intervenants[index_intervenant].save();
+                heure_CM.push(somme_heure_CM);
+                heure_TD.push(somme_heure_TD);
+                heure_TP.push(somme_heure_TP);
+                heure_Partiel.push(somme_heure_Partiel);
 
-        intervenant_decomposition.nombre_heure_CM = heure_CM;
-        intervenant_decomposition.nombre_heure_TD = heure_TD;
-        intervenant_decomposition.nombre_heure_TP = heure_TP;
-        intervenant_decomposition.nombre_heure_Partiel = heure_Partiel;
-        return projet.save();
+                intervenant_decomposition.nombre_heure_CM = heure_CM;
+                intervenant_decomposition.nombre_heure_TD = heure_TD;
+                intervenant_decomposition.nombre_heure_TP = heure_TP;
+                intervenant_decomposition.nombre_heure_Partiel = heure_Partiel;
+                return projet.save();
+            });
+        });
     }).then(() => {
         res.redirect('/projet/decomposition/'+ req.params.idProjet +'/'+ req.params.idFormation);
     }, err => console.log(err));
