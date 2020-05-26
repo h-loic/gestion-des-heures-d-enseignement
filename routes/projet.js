2621e02e5106e8d173e06e3665ada7b0b473fad2
h-loic <h-loic@orange.fr>
2020-05-26 09:56:50 +0200

    debut clonage

--- routes/projet.js
+++ routes/projet.js
@@ -5,6 +5,25 @@ var Enseignant = require('./../models/Enseignant');
 var Statut = require('./../models/Statut');
 var Intervenant = require('./../models/Intervenant');
 
+router.post('/projet/clone', (req, res) => {
+    Projet.findById(req.body.projet._id).then(projet => {
+        /*
+        console.log("1");
+        let projet_clone = new Projet(projet);
+        console.log("2");
+        projet_clone.isNew = true;
+        console.log("3");
+        objectIdDel(projet_clone);
+        console.log("4");
+        projet_clone.nom = req.body.nom;
+        console.log("5");
+        projet_clone.save();
+        console.log("6");*/
+    }).then(() => {
+        res.redirect('/accueil');
+    }, err => console.log(err));
+});
+
 router.get('/projet/bilan/:idProjet/', (req,res) => {
     Projet.findById(req.params.idProjet).populate('intervenants').then(projet =>{
         projet.populate('intervenants.enseignant').execPopulate().then(projet => {
@@ -86,6 +105,14 @@ router.get('/projet/new', (req,res) => {
     });
 });
 
+router.get('/projet/clone', (req,res) => {
+    Projet.find({}).then(projets =>{
+        var projet = new Projet();
+        res.render('projets/clone.html', { projet : projet, projets : projets, endpoint: '/projet/clone'});
+    });
+});
+
+
 router.get('/projet/edit/:id', (req,res) => {
     Formation.find({}).then(formations =>{
         Projet.findById(req.params.id).then(projet => {
@@ -93,6 +120,8 @@ router.get('/projet/edit/:id', (req,res) => {
         });
     });
 });
+
+
 
 router.get('/projet/delete/:id', (req, res) => {
     Projet.findOneAndRemove({ _id : req.params.id}).then(() => {
@@ -125,4 +154,24 @@ router.get('/projet/:id', (req, res) => {
     });
 });
 
+var objectIdDel = function(copiedObjectWithId) {
+    if (copiedObjectWithId != null && typeof(copiedObjectWithId) != 'string' &&
+        typeof(copiedObjectWithId) != 'number' && typeof(copiedObjectWithId) != 'boolean' ) {
+        //for array length is defined however for objects length is undefined
+        if (typeof(copiedObjectWithId.length) == 'undefined') {
+            delete copiedObjectWithId._id;
+            for (var key in copiedObjectWithId) {
+                objectIdDel(copiedObjectWithId[key]); //recursive del calls on object elements
+            }
+        }
+        else {
+            for (var i = 0; i < copiedObjectWithId.length; i++) {
+                objectIdDel(copiedObjectWithId[i]);  //recursive del calls on array elements
+            }
+        }
+    }
+    return "ok";
+}
+
 module.exports = router;
+
