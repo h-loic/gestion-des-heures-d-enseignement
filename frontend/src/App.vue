d6533677466418136870e28e3b0ba17d03959b3e
h-loic <h-loic@orange.fr>
2020-05-27 11:14:58 +0200

    creation vue Accueil

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav class="navbar navbar-light bg-light">
       <router-link class="navbar-brand" to="/">
-        <img src="/frontend/public/iut.ico" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img src="/frontend/src/assets/iut.ico" width="30" height="30" class="d-inline-block align-top" alt="">
         gestion des heures d'enseignement
       </router-link>
     </nav>
---
+++ frontend/src/views/Accueil.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-around mt-4 mb-4">
+      <router-link to="/formation" type="button" class="btn btn-outline-info col-md-6 mt-1 mb-1">gestion des formations</router-link>
+      <router-link to="/enseignant" type="button" class="btn btn-outline-info col-md-6 mt-1 mb-1">gestion des enseignants</router-link>
+    </div>
+    <p class="h1 justify-content-start mt-3 mb-3">liste des projets</p>
+    <div class="row justify-content-around">
+      <router-link to="/projet/new" type="button" class="btn btn-outline-info col-md-5 mt-3 mb-1">creer un nouveau projet</router-link>
+    </div>
+    <div class="row justify-content-around">
+      <router-link to="/projet/clone" type="button" class="btn btn-outline-info col-md-5 mt-1 mb-5">cloner un  projet</router-link>
+    </div>
+    <div class="row">
+      <router-link to="/accueil-date" type="button" class="btn btn-success col-2 m-1">trier par date</router-link>
+      <router-link to="/accueil-nom" type="button" class="btn btn-success col-2 m-1">trier par nom</router-link>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Accueil',
+}
+</script>
